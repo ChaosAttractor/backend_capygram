a9c6069e5ff60e0652cd918b9466c853b5358459
Данила И. Стебаков <danila.stebakov@areal.company>
2023-08-11 12:37:48 +0300

    feat: prisma init, .env.example, .editorconfig

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api/v1');
+  app.useStaticAssets(join(__dirname, '..', '/src/assets'), {
+    prefix: '/api/assets',
+  });
+
+  const port = process.env.PORT || 8080;
+
+  await app.listen(port);
 };
 bootstrap();
